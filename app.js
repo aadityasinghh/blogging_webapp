a6a41668218ca74606621cad5e5f8fb1e1d94b62
ADITYA SINGH <adityasingh20102001@gmail.com>
2024-01-24 23:10:30 +0530

    made changes for hositing

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const http = require("http");
 
+const port = process.env.PORT || 3000;
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -61,6 +63,6 @@ app.post("/compose", function (req, res) {
   posts.push(post);
   res.redirect("/");
 });
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
